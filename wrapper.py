bfd6e8e337551d6ccc1d1f57f4dcbf36140adeb8
57866696+thewooniverse@users.noreply.github.com <Joo@Wooyoungs-MacBook-Pro.local>
2020-12-27 02:10:56 +0000

    wrapper start

---
+++ wrapper.py
@@ -0,0 +1,13 @@
+from decryption import combination_decrypt as decrypt
+from encryption import combination_encrypt as encrypt
+
+# Basic test for functionality after import
+# test_str_key = "FUNFUNRUNRUN"
+# test_num_key = "023654"
+# test_pt = "yooow1233hatisthepassword????"
+# print(test_pt)
+# tst_ct = (encrypt(test_pt, test_str_key, test_num_key, 3, 2))
+# print(tst_ct)
+# print(decrypt(tst_ct, test_str_key, test_num_key, 3, 2))
+
+
