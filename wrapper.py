64456945a2a437c1d5236e528448eef9ee95e4fc
57866696+thewooniverse@users.noreply.github.com <Joo@wooyoungs-mbp.home>
2021-01-07 13:47:39 +0000

    addl wrapper logic with os, sys, shutil

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,54 @@
+#! /usr/bin/python
+# wrapper.py for Password Protector - is a script for using the decryption, encryption logic to process files.
+# usage:
+#   wrapper.py lock filename - locks filename to produce a locked_filename
+#   wrapper.py unlock filename - unlocks a filename to product a filename
+#   wrapper.py lock filename D - locks filename but also deleted
+
+import os, sys, pyperclip, shutil
 from decryption import combination_decrypt as decrypt
 from encryption import combination_encrypt as encrypt
 
+
+# read sys argv
+
+
+# open and delete.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### testing ###
 # Basic test for functionality after import
 # test_str_key = "FUNFUNRUNRUN"
 # test_num_key = "023654"
